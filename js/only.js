e1ef37b0204fbe4e1545cf746e57b3f00b10182c
shahriarmostafa <sahirmiashab@gmail.com>
2023-11-06 20:30:33 +0600

    now  you are talking

--- js/only.js
+++ js/only.js
@@ -1,6 +1,23 @@
+// drop down
+
 const dropdown = document.getElementsByClassName("dropdown")[0];
 const profilePic = document.getElementsByClassName("profile-pic")[0];
 function dropDownFunc(){
     dropdown.classList.toggle('drop-down-opacity');
 }
 profilePic.addEventListener("click", dropDownFunc);
+ 
+
+
+// night videw
+
+const section = document.getElementsByTagName("section")[0];
+const switcher = document.getElementsByClassName("slider")[0];
+
+
+function nightMode(){
+    section.classList.toggle("day-view");
+    section.classList.toggle("night-view");
+}
+switcher.addEventListener("click", nightMode);
+
