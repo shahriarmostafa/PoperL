3f4e39fe5bea44556771ec92d5453b75c1b4faba
shahriarmostafa <sahirmiashab@gmail.com>
2025-01-11 01:52:52 +0600

    completed chat messages

--- src/interfaces/Private/Chat/IncomingMessage.jsx
+++ src/interfaces/Private/Chat/IncomingMessage.jsx
@@ -1,15 +1,18 @@
-import img from '../../../assests/328039808_553574763481911_1410511776054264885_n.jpg';
 
-export default function IncomingMessage ({message}){
+export default function IncomingMessage ({message, image}){
     return (
         <div className="incoming-message">
             <div className="info">
                 <div className="name">
                     <span>Rahim Hasan</span>
                 </div>
-                <div className="photo">
-                    <img className="sent-image" src={img} alt=""></img>
-                </div>
+                {
+                    image?<div className="photo">
+                        <img className="sent-image" src={img} alt=""></img>
+                    </div>
+                : ''
+                }
+                
                 <div className="message">
                     <span>{message}</span>
                 </div>
--- src/interfaces/Private/Chat/SentMessage.jsx
+++ src/interfaces/Private/Chat/SentMessage.jsx
@@ -1,14 +1,16 @@
-import img from '../../../assests/328039808_553574763481911_1410511776054264885_n.jpg';
 
 export default function SentMessage({message, image}) {
     return (
         <div className="my-message">
             <div className="info">
-                <div className="photo">
+                
                     {
-                        image? <img className="sent-image" src={image} alt=""></img>: ''
+                        image?
+                        <div className="photo">
+                            <img className="sent-image" src={image} alt=""></img>
+                        </div>
+                         : ''
                     }
-                </div>
                 <div className="message">
                     <span>{message}</span>
                 </div>
