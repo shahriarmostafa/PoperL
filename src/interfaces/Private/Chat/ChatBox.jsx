d80ec224098819b74d698711c654fabc49271cc8
shahriarmostafa <sahirmiashab@gmail.com>
2025-01-11 13:21:09 +0600

    chat focus fix

--- src/interfaces/Private/Chat/ChatBox.jsx
+++ src/interfaces/Private/Chat/ChatBox.jsx
@@ -6,6 +6,7 @@ import { arrayUnion, doc, getDoc, onSnapshot, updateDoc } from 'firebase/firesto
 import {db} from '../../../firebase/firebase.init';
 import { getChatBoxData } from '../../../Hooks/getChatBoxData';
 import { AuthContext } from '../../../providers/AuthProvider';
+import { IoSend } from 'react-icons/io5';
 
 export default function ChatBox(){
 
@@ -132,8 +133,8 @@ export default function ChatBox(){
                                 <div className="inbox d-flex">
                                     <input autoComplete="off" ref={inputRef} onKeyUp ={handleInputChange} name="message" type="text" placeholder="Enter your message here..."></input>
                                 </div>
-                                <button className='send' onClick={handleMessageSend}>                                
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M498.1 5.6c10.1 7 15.4 19.1 13.5 31.2l-64 416c-1.5 9.7-7.4 18.2-16 23s-18.9 5.4-28 1.6L284 427.7l-68.5 74.1c-8.9 9.7-22.9 12.9-35.2 8.1S160 493.2 160 480V396.4c0-4 1.5-7.8 4.2-10.7L331.8 202.8c5.8-6.3 5.6-16-.4-22s-15.7-6.4-22-.7L106 360.8 17.7 316.6C7.1 311.3 .3 300.7 0 288.9s5.9-22.8 16.1-28.7l448-256c10.7-6.1 23.9-5.5 34 1.4z"/></svg>
+                                <button tabIndex="-1" className='send' onClick={handleMessageSend}>                                
+                                    <IoSend></IoSend>
                                 </button>
                         </div>
                     </div>
