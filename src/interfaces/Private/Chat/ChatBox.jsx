7b4c1b24aa6f1913c563f8cf76ac35af508448d6
shahriarmostafa <sahirmiashab@gmail.com>
2025-01-11 13:48:38 +0600

    this is it for today

--- src/interfaces/Private/Chat/ChatBox.jsx
+++ src/interfaces/Private/Chat/ChatBox.jsx
@@ -41,11 +41,10 @@ export default function ChatBox(){
     
 
     const handleInputChange = (event) => {
-        setText(event.target.value);
-        if(event.key == 'Enter'){
+        event.preventDefault();
+        event.target.message.focus();
+        setText(event.target.message.value);
             handleMessageSend()
-            
-        }
     };
     
     
@@ -120,7 +119,7 @@ export default function ChatBox(){
                             </div>
                             <div ref={endRef}></div>
                         </div>
-                        <div className="typing-area d-flex">
+                        <div className="typing-area d-flex justify-content-center">
                             <div className="select-image d-flex">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M0 96C0 60.7 28.7 32 64 32H448c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96zM323.8 202.5c-4.5-6.6-11.9-10.5-19.8-10.5s-15.4 3.9-19.8 10.5l-87 127.6L170.7 297c-4.6-5.7-11.5-9-18.7-9s-14.2 3.3-18.7 9l-64 80c-5.8 7.2-6.9 17.1-2.9 25.4s12.4 13.6 21.6 13.6h96 32H424c8.9 0 17.1-4.9 21.2-12.8s3.6-17.4-1.4-24.7l-120-176zM112 192a48 48 0 1 0 0-96 48 48 0 1 0 0 96z"/></svg>
                             </div>
@@ -130,12 +129,14 @@ export default function ChatBox(){
                             <div className="voice-message d-flex">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M192 0C139 0 96 43 96 96l0 160c0 53 43 96 96 96s96-43 96-96l0-160c0-53-43-96-96-96zM64 216c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 40c0 89.1 66.2 162.7 152 174.4l0 33.6-48 0c-13.3 0-24 10.7-24 24s10.7 24 24 24l72 0 72 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-48 0 0-33.6c85.8-11.7 152-85.3 152-174.4l0-40c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 40c0 70.7-57.3 128-128 128s-128-57.3-128-128l0-40z"/></svg>
                             </div>
-                                <div className="inbox d-flex">
-                                    <input autoComplete="off" ref={inputRef} onKeyUp ={handleInputChange} name="message" type="text" placeholder="Enter your message here..."></input>
-                                </div>
-                                <button tabIndex="-1" className='send' onClick={handleMessageSend}>                                
-                                    <IoSend></IoSend>
-                                </button>
+                                <form className='d-flex' onSubmit={handleInputChange}>
+                                    <div className="inbox d-flex">
+                                        <input autoComplete="off" ref={inputRef} name="message" type="text" placeholder="Enter your message here..."></input>
+                                    </div>
+                                    <button type="submit" tabIndex="-1" className='send'>                                
+                                        <IoSend></IoSend>
+                                    </button>
+                                </form>
                         </div>
                     </div>
                 </section>
