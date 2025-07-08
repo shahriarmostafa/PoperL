5eb9d31e0a7ca7d30ef7c999174c9d47a0c2fd24
shahriarmostafa <sahirmiashab@gmail.com>
2025-07-08 18:18:04 +0600

    fixed the call

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosSecure = axios.create({
-    // baseURL: "http://backend-eta-blue-92.vercel.app"})
-    baseURL: "http://localhost:5000"})
+    baseURL: "https://backend-yege.onrender.com"})
+    // baseURL: "http://localhost:5000"})
 
 
 const useAxiosSecure = () => {
